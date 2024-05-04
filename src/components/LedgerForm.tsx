07298cc801cb99990589cba961d5a88e7b3075a7
tijo <81065469+artijo@users.noreply.github.com>
2024-05-04 16:28:31 +0700

    fix ts

--- src/components/LedgerForm.tsx
+++ src/components/LedgerForm.tsx
@@ -1,7 +1,7 @@
 import { useForm, SubmitHandler } from "react-hook-form"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import { Button } from "./ui/button"
 export default function LedgerForm() {
     type Inputs = {
         title: string
