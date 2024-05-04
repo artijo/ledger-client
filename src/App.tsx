6652e7dd9e17ca4c911e055bdc92905a6195762c
tijo <81065469+artijo@users.noreply.github.com>
2024-05-04 20:43:32 +0700

    chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import LedgerForm from "./components/LedgerForm"
 import { Routes, Route } from 'react-router-dom'
 import Layout from "./Layout";
+import Home from "./pages/Home";
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<LedgerForm />} />
+        <Route path="/" element={<Home />} />
         <Route path="/add" element={<LedgerForm />} />
       </Routes>
     </Layout>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
+import ChartExpend from "../components/ChartExpend";
+
 const Home = () => {
     return (
-        <div>
-        <h1>Home</h1>
-        </div>
+        <>
+            <div className="flex gap-4">
+                <div>
+                    Today Expend
+                    <p>75</p>
+                </div>
+                <div>
+                    Expend(Mounth)
+                    <p>500</p>
+                </div>
+                <div>
+                    Income
+                    <p>1000</p>
+                </div>
+                <div>
+                    Balance
+                    <p>500</p>
+                </div>
+            </div>
+            <ChartExpend />
+        </>
     );
     };
 
