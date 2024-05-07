5ffb90bb06b819e27b0203ab933bf0fc9d09a9b1
tijo <81065469+artijo@users.noreply.github.com>
2024-05-07 21:46:52 +0700

    Update ChartExpend.tsx

--- src/components/ChartExpend.tsx
+++ src/components/ChartExpend.tsx
@@ -1,3 +1,5 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,6 +11,7 @@ import {
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -18,61 +21,50 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-  
-  },
+type ThisMonth = {
+  day: number;
+  income: number;
+  expense: number;
 };
 
-var data = [
-  { day: 1, value: 10 },
-  { day: 2, value: 20 },
-  { day: 3, value: 30 },
-  { day: 4, value: 40 },
-  { day: 5, value: 50 },
-  { day: 6, value: 60 },
-  { day: 7, value: 70 },
-  { day: 8, value: 80 },
-  { day: 9, value: 90 },
-  { day: 10, value: 100 },
-  { day: 11, value: 110 },
-  { day: 12, value: 120 },
-  { day: 13, value: 130 },
-  { day: 14, value: 140 },
-  { day: 15, value: 150 },
-  { day: 16, value: 160 },
-  { day: 17, value: 170 },
-  { day: 18, value: 180 },
-  { day: 19, value: 190 },
-  { day: 20, value: 200 },
-  { day: 21, value: 210 },
-  { day: 22, value: 220 },
-  { day: 23, value: 230 },
-  { day: 24, value: 240 },
-  { day: 25, value: 250 },
-  { day: 26, value: 260 },
-  { day: 27, value: 270 },
-  { day: 28, value: 280 },
-  { day: 29, value: 290 },
-  { day: 30, value: 300 },
-  { day: 31, value: 310 },
-];
-export const dataobj = {
-  labels: data.map((item) => item.day),
-  datasets: [
-    {
-      label: "Expenditure",
-      data: data.map((item) => item.value),
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-    },
-  ],
-};
 
 const ChartExpend = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+    
+    },
+  };
+  
+  const { data } = useQuery<ThisMonth[]>({
+    queryKey: ["chartthismouth"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:4000/ledgers/thismonth", {
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRpam8ubWVAaG90bWFpbC5jb20iLCJpYXQiOjE3MTQ1NzMzNTF9.5haaboIVOqH5Xr8t2QwFl0HXUdm6QDfsH3siQCYQ76k`,
+        },
+      });
+      return res.data;
+    },
+  });
+  const dataobj = {
+    labels: data?.map((item) => item.day),
+    datasets: [
+      {
+        label: "Expenditure",
+        data: data?.map((item) => item.expense),
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+      {
+        label: "Income",
+        data: data?.map((item) => item.income),
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+      },
+    ],
+  };
   return (
     <>
       <Bar options={options} data={dataobj} />
